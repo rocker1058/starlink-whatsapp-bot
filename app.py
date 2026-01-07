260073ac143b45610196dbff302e6aa03aed3b97
Leonardo Alberto Arango Cifuentes <leonardo.arango@carvajal.com>
2026-01-07 18:00:46 -0500

    Leer Google Sheets

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/etc/secrets/credentials.json"
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import gspread
 
 app = Flask(__name__)
+
+SHEET_ID = "1XG6HxVpxMD1HP4sIxCRM4EXCLpVYRReEOHYGqRd0tyM"
+SHEET_NAME = "clientes_bot"
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp():
     resp = MessagingResponse()
-    resp.message("Bot activo ✅")
+
+    try:
+        # Conectar a Google Sheets
+        gc = gspread.service_account()
+        sh = gc.open_by_key(SHEET_ID)
+        worksheet = sh.worksheet(SHEET_NAME)
+
+        filas = worksheet.get_all_records()
+
+        resp.message(
+            f"Conectado a Google Sheets ✅\n"
+            f"Filas leídas: {len(filas)}"
+        )
+
+    except Exception as e:
+        resp.message(
+            f"Error al leer Google Sheets ❌\n"
+            f"{str(e)}"
+        )
+
     return str(resp)
 
 if __name__ == "__main__":
