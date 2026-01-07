9678957df53c2c99e73fbaa7dd54c79d9a055ba6
Leonardo Alberto Arango Cifuentes <leonardo.arango@carvajal.com>
2026-01-07 17:39:40 -0500

    Bot WhatsApp inicial

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 @app.route("/whatsapp", methods=["POST"])
 def whatsapp():
-    mensaje = request.form.get("Body")
-    return "Bot activo ✅"
+    resp = MessagingResponse()
+    resp.message("Bot activo ✅")
+    return str(resp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
